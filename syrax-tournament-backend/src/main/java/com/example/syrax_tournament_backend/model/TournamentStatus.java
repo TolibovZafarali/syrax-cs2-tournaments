fc743e40130e1e5d26e5ffacd23998ec8c30b441
Zafarali Tolibov <tolibovzafarali7@gmail.com>
2025-07-09 22:53:30 -0500

    Installed Axios for frondend and backend intergration

--- syrax-tournament-backend/src/main/java/com/example/syrax_tournament_backend/model/TournamentStatus.java
+++ syrax-tournament-backend/src/main/java/com/example/syrax_tournament_backend/model/TournamentStatus.java
@@ -1,5 +1,8 @@
 package com.example.syrax_tournament_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum TournamentStatus {
     UPCOMING,
     ONGOING,
